fb2d122d349e8f53923cbcd11341872b8e1e0426
mike4747 <mike4747@oh.rr.com>
2019-03-31 20:57:45 -0400

    homework finished pending any problems discovered

--- liri.js
+++ liri.js
@@ -1,15 +1,12 @@
 "use strict";
 
 require("dotenv").config();
-
 var axios = require("axios");
-
 var fs = require("fs");
-
+var moment = require("moment");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-
 var movieName = "";
 var artistName = "";
 var songName = "";
@@ -19,6 +16,8 @@ var omdbURL = "";
 var dataArray = [];
 var formattedString = "";
 var lim = 0;
+var formattedDate = "";
+var secLoc = "";
 
 function concertThis(txt) {
     if (txt || process.argv[3]) {
@@ -37,16 +36,22 @@ function concertThis(txt) {
         axios.get(bitURL).then(
             function (response) {
                 console.log("\n");
-                if (response.data.length > 0) {
+                if (response.data[0].venue) {
                     console.log("Showing BandsInTown info for: '" + artistName.replace("+", " ") + "' concerts.\n");
                     response.data.forEach(obj => {
                         console.log("----------------------------------\n");
                         console.log(obj.venue.name);
-                        console.log(obj.venue.city + ", " + obj.venue.region);
-                        console.log(obj.datetime + "\n");
+                        if (obj.venue.region == "") {
+                            secLoc = obj.venue.country;
+                        } else {
+                            secLoc = obj.venue.region;
+                        }
+                        console.log(obj.venue.city + ", " + secLoc);
+                        formattedDate = moment(obj.datetime, "YYYY-MM-DDThh:mm:ss").format("MM/DD/YYYY");
+                        console.log(formattedDate + "\n");
                     });
                 } else {
-                    console.log("\n\nNo BandsInTown shows found for the band/artist: '" + artistName.replace("+", " ") + "'");
+                    console.log("No BandsInTown shows found for the band/artist: '" + artistName.replace("+", " ") + "'\n");
                 }
             }
         )
@@ -84,7 +89,7 @@ function spotifyThis(txt) {
                         console.log("Album Name: " + obj.album.name + "\n");
                     });
                 } else {
-                    console.log("\nNo Spotify results found for the song: '" + songName.replace("+", " ") + "'");
+                    console.log("No Spotify results found for the song: '" + songName.replace("+", " ") + "'\n");
                 }
             })
             .catch(function (err) {
@@ -94,7 +99,7 @@ function spotifyThis(txt) {
         lim = 1;
         defSongName = "'The Sign' by 'Ace of Bass'";
         songId = "0hrBpAOgrt8RXigk83LLNE";
-        console.log("\n\nYou didn't enter a song name, so we're showing you info about: " + defSongName + "\n");
+        console.log("\nYou didn't enter a song name, so we're showing you info about: " + defSongName + "\n");
 
         spotify
             .request("https://api.spotify.com/v1/tracks/" + songId + "?offset=1&limit=" + lim)
@@ -122,15 +127,15 @@ function movieThis(txt) {
             }
         }
     } else {
-        movieName = "Mr. Nobody";
-        console.log("\n\nYou didn't enter a movie name, so we're showing you info about: '" + movieName.replace("+", " ") + "'");
+        movieName = "Mr.+Nobody";
+        console.log("\nYou didn't enter a movie name, so we're showing you info about: '" + movieName.replace("+", " ") + "'");
     }
     omdbURL = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
     axios.get(omdbURL).then(
         function (response) {
             console.log("\n");
             if (response.data.Response == "True") {
-                console.log("\nShowing OMDB match for: '" + movieName + "'.\n\n----------------------------------\n");
+                console.log("Showing the first OMDB match for: '" + movieName.replace("+", " ") + "'.\n\n----------------------------------\n");
                 console.log("Title: " + response.data.Title + "\n");
                 console.log("Year of release is: " + response.data.Year + "\n");
                 console.log("IMDB rating: " + response.data.Ratings[0].Value + "\n");
@@ -140,7 +145,7 @@ function movieThis(txt) {
                 console.log("Movie plot: " + response.data.Plot + "\n");
                 console.log("Actors: " + response.data.Actors + "\n");
             } else {
-                console.log("\n\nNo movie was found at OMDB matching your search criteria: '" + movieName.replace("+", " ") + "'");
+                console.log("No movie was found at OMDB matching your search criteria: '" + movieName.replace("+", " ") + "'\n");
             }
         }
     )
@@ -173,4 +178,4 @@ if (process.argv[2] === "concert-this") {
 
 // spotify-this-song,"I Want it That Way"
 // movie-this,"Blues Brothers"
-// concert-this,"Drake"
+// concert-this,"Fleetwood Mac"
