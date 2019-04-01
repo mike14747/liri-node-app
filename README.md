16e823eb0deb6eef62dadcdf13afdcdc4a91a123
mike4747 <mike4747@oh.rr.com>
2019-04-01 16:08:17 -0400

    added logging to log.txt and started a readme file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # liri-node-app
+
+* The user will input 'node liri' plus one of these 4 possible second parameters:
+  * concert-this
+    * this parameter requires an additional artist/band parameter
+    * sample of this completed input would be: 'node liri concert-this grand funk'
+  * movie-this
+    * this parameter requires an additional movie name parameter
+    * without this additional parameter, results for the movie 'Mr. Nobody' will be displayed
+    * sample of this completed input would be: 'node liri movie-this blues brothers'
+  * spotify-this-song
+    * this parameter requires an additional song name parameter
+    * without this additional parameter, results for the song 'The Sign' by 'Ace of Bass' will be displayed
+    * sample of this completed input would be: 'node liri spotify-this-song carouselambra'
+  * do-what-it-says
+    * this parameter cause liri to ignore any additional parameters
+    * it will read the file 'randon.txt'... whose contents include one of the first 3 parameters, plus an additional one
+
+* 
--- liri.js
+++ liri.js
@@ -19,6 +19,14 @@ var lim = 0;
 var formattedDate = "";
 var secLoc = "";
 var defSongName = "";
+var logStringPre = "\r\n-------------------------------------------------------\r\nSearch Criteria and Results:\r\n-------------------------------------------------------";
+var logString = "";
+
+function logData(logThis) {
+    console.log(logThis);
+    logStringPre += logThis;
+    fs.appendFile("log.txt", logStringPre, function (err) { });
+}
 
 function concertThis(txt) {
     if (txt || process.argv[3]) {
@@ -37,31 +45,35 @@ function concertThis(txt) {
         axios.get(bitURL)
             .then(
                 function (response) {
-                    console.log("\n");
+                    logString += "\r\n\r\n";
                     if (response.status === 200 && response.data[0] && response.data[0].venue) {
-                        console.log("Showing BandsInTown info for: '" + artistName.replace(/\+/g, " ") + "' concerts.\n");
+                        logString += "Showing BandsInTown info for: '" + artistName.replace(/\+/g, " ") + "' concerts.\r\n\r\n";
                         response.data.forEach(obj => {
-                            console.log("----------------------------------\n");
-                            console.log(obj.venue.name);
+                            logString += "----------------------------------\r\n\r\n";
+                            logString += obj.venue.name + "\r\n";
                             if (obj.venue.region == "") {
                                 secLoc = obj.venue.country;
                             } else {
                                 secLoc = obj.venue.region;
                             }
-                            console.log(obj.venue.city + ", " + secLoc);
+                            logString += obj.venue.city + ", " + secLoc + "\r\n";
                             formattedDate = moment(obj.datetime, "YYYY-MM-DDThh:mm:ss").format("MM/DD/YYYY");
-                            console.log(formattedDate + "\n");
+                            logString += formattedDate + "\r\n\r\n";
                         });
+                        logData(logString);
                     } else {
-                        console.log("No BandsInTown shows found for the band/artist: '" + artistName.replace(/\+/g, " ") + "'\n");
+                        logString += "No BandsInTown shows found for the band/artist: '" + artistName.replace(/\+/g, " ") + "'\r\n\r\n";
+                        logData(logString);
                     }
                 }
             )
             .catch(function (error) {
-                console.log("\nAn error occurred... please try your search again.");
+                logString += "\r\n\r\nAn error occurred... please try your search again.";
+                logData(logString);
             });
     } else {
-        console.log("\n\nYou didn't enter band/artist name.\n");
+        logString += "\r\n\r\nYou didn't enter band/artist name.\r\n\r\n";
+        logData(logString);
         return;
     }
 }
@@ -83,39 +95,44 @@ function spotifyThis(txt) {
         spotify
             .search({ type: 'track', query: songName, limit: lim })
             .then(function (response) {
-                console.log("\n");
+                logString += "\r\n\r\n";
                 if (response.tracks.total > 0) {
-                    console.log("\nShowing Spotify info for (up to 5) matches for: '" + songName.replace(/\+/g, " ") + "'.\n");
+                    logString += "Showing Spotify info for (up to 5) matches for: '" + songName.replace(/\+/g, " ") + "'.\r\n\r\n";
                     response.tracks.items.forEach(obj => {
-                        console.log("----------------------------------\n");
-                        console.log("Artist: " + obj.artists[0].name);
-                        console.log("Song Name: " + obj.name);
-                        console.log("Spotify preview link: " + obj.preview_url);
-                        console.log("Album Name: " + obj.album.name + "\n");
+                        logString += "----------------------------------\r\n\r\n";
+                        logString += "Artist: " + obj.artists[0].name + "\r\n";
+                        logString += "Song Name: " + obj.name + "\r\n";
+                        logString += "Spotify preview link: " + obj.preview_url + "\r\n";
+                        logString += "Album Name: " + obj.album.name + "\r\n\r\n";
                     });
+                    logData(logString);
                 } else {
-                    console.log("No Spotify results found for the song: '" + songName.replace(/\+/g, " ") + "'\n");
+                    logString += "No Spotify results found for the song: '" + songName.replace(/\+/g, " ") + "'\r\n\r\n";
+                    logData(logString);
                 }
             })
             .catch(function (err) {
-                console.log(err);
+                logString += err;
+                logData(logString);
             });
     } else {
         lim = 1;
         defSongName = "'The Sign' by 'Ace of Bass'";
         songId = "0hrBpAOgrt8RXigk83LLNE";
-        console.log("\nYou didn't enter a song name, so we're showing you info about: " + defSongName + "\n");
+        logString += "\r\n\r\nYou didn't enter a song name, so we're showing you info about: " + defSongName + "\r\n\r\n";
 
         spotify
             .request("https://api.spotify.com/v1/tracks/" + songId + "?offset=1&limit=" + lim)
             .then(function (data) {
-                console.log("Artist: " + data.artists[0].name);
-                console.log("Song Name: " + data.name);
-                console.log("Spotify preview link: " + data.preview_url);
-                console.log("Album Name: " + data.album.name + "\n");
+                logString += "Artist: " + data.artists[0].name + "\r\n";
+                logString += "Song Name: " + data.name + "\r\n";
+                logString += "Spotify preview link: " + data.preview_url + "\r\n";
+                logString += "Album Name: " + data.album.name + "\r\n\r\n";
+                logData(logString);
             })
             .catch(function (err) {
                 console.error('Error occurred: ' + err);
+                logData(logString);
             });
     }
 }
@@ -133,24 +150,26 @@ function movieThis(txt) {
         }
     } else {
         movieName = "Mr.+Nobody";
-        console.log("\n\nYou didn't enter a movie name, so we're showing you info about: '" + movieName.replace(/\+/g, " ") + "'");
+        logString += "\r\n\r\nYou didn't enter a movie name, so we're showing you info about: '" + movieName.replace(/\+/g, " ") + "'\r\n";
     }
     omdbURL = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
     axios.get(omdbURL).then(
         function (response) {
-            console.log("\n");
+            logString += "\r\n\r\n";
             if (response.data.Response == "True") {
-                console.log("Showing the first OMDB match for: '" + movieName.replace(/\+/g, " ") + "'.\n\n----------------------------------\n");
-                console.log("Title: " + response.data.Title + "\n");
-                console.log("Year of release is: " + response.data.Year + "\n");
-                console.log("IMDB rating: " + response.data.Ratings[0].Value + "\n");
-                console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value + "\n");
-                console.log("Country of production: " + response.data.Country + "\n");
-                console.log("Language: " + response.data.Language + "\n");
-                console.log("Movie plot: " + response.data.Plot + "\n");
-                console.log("Actors: " + response.data.Actors + "\n");
+                logString += "Showing the first OMDB match for: '" + movieName.replace(/\+/g, " ") + "'.\r\n\r\n----------------------------------\r\n\r\n";
+                logString += "Title: " + response.data.Title + "\r\n\r\n";
+                logString += "Year of release is: " + response.data.Year + "\r\n\r\n";
+                logString += "IMDB rating: " + response.data.Ratings[0].Value + "\r\n\r\n";
+                logString += "Rotten Tomatoes rating: " + response.data.Ratings[1].Value + "\r\n\r\n";
+                logString += "Country of production: " + response.data.Country + "\r\n\r\n";
+                logString += "Language: " + response.data.Language + "\r\n\r\n";
+                logString += "Movie plot: " + response.data.Plot + "\r\n\r\n";
+                logString += "Actors: " + response.data.Actors + "\r\n\r\n";
+                logData(logString);
             } else {
-                console.log("No movie was found at OMDB matching your search criteria: '" + movieName.replace(/\+/g, " ") + "'\n");
+                logString += "No movie was found at OMDB matching your search criteria: '" + movieName.replace(/\+/g, " ") + "'\r\n\r\n";
+                logData(logString);
             }
         }
     )
@@ -165,7 +184,7 @@ if (process.argv[2] === "concert-this") {
 } else if (process.argv[2] === "do-what-it-says") {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
-            return console.log(error);
+            return logData(error);
         }
         dataArray = data.split(",");
         formattedString = dataArray[1].slice(1, dataArray[1].length - 1).replace(/ /g, "+");
@@ -178,5 +197,6 @@ if (process.argv[2] === "concert-this") {
         }
     });
 } else {
-    console.log("\nYour search type is not supported.\n\nPlease use only 'concert-this', 'spotify-this-song', 'movie-this' or 'do-what-it-says' as the first parameter");
+    logString += "\r\n\r\nYour search type is not supported.\r\n\r\nPlease use only 'concert-this', 'spotify-this-song', 'movie-this' or 'do-what-it-says' as the first parameter\r\n\r\n";
+    logData(logString);
 }
